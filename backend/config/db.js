d99352333bfef17b51ba4fb909e3e89d74373e07
Sunil Chouhan <chouhansunilm@gmail.com>
2026-05-30 20:09:33 +0530

    Enable SSl in db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
-const { Pool } = require('pg');
-
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
